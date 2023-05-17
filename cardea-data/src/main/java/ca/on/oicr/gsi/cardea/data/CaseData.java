b1a40706a13720dcd00da935876a698132ac14ee
Heather Armstrong <heather.armstrong@oicr.on.ca>
2023-05-17 15:00:32 -0400

    Re-add assaysById to CaseData

--- cardea-data/src/main/java/ca/on/oicr/gsi/cardea/data/CaseData.java
+++ cardea-data/src/main/java/ca/on/oicr/gsi/cardea/data/CaseData.java
@@ -5,21 +5,29 @@ import static java.util.Objects.requireNonNull;
 import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Immutable CaseData
  */
 public class CaseData {
 
+  private final Map<Long, Assay> assaysById;
   private final List<Case> cases;
   private final List<OmittedSample> omittedSamples;
   private final ZonedDateTime timestamp;
 
-  public CaseData(List<Case> cases, List<OmittedSample> omittedSamples, ZonedDateTime timestamp) {
+  public CaseData(List<Case> cases, List<OmittedSample> omittedSamples, Map<Long, Assay> assaysById,
+      ZonedDateTime timestamp) {
+    this.assaysById = Collections.unmodifiableMap(assaysById);
     this.cases = unmodifiableList(cases);
     this.omittedSamples = Collections.unmodifiableList(omittedSamples);
     this.timestamp = requireNonNull(timestamp);
 
+  }
+
+  public Map<Long, Assay> getAssaysById() {
+    return assaysById;
   }
 
   public List<Case> getCases() {
